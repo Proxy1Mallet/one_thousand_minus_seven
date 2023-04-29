e6a60acb5d11bff0427595ce047bcda38690c2c0
Proxy Mallet <qq84063@gmail.com>
2023-04-29 21:38:59 +0500

    one_thousand_minus_seven

---
+++ one_thousand_minus_seven.js
@@ -0,0 +1,5 @@
+var thousand = 1000
+while (thousand >= 0) {
+    console.log(`${thousand} - 7 = ${thousand-=7}`)
+}
+console.log('Я гуль.')
---
+++ one_thousand_minus_seven.py
@@ -0,0 +1,5 @@
+thousand = 1000
+while thousand >= 0:
+    print(f'{thousand} - 7 = {thousand - 7}')
+    thousand -= 7
+print('Я гуль.')
